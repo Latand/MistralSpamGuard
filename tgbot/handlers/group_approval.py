20d155f6dfdeb9f7aa68aa5d0f771333abaa9fdb
latand <latand666@gmail.com>
2024-02-06 22:56:52 +0100

    Update group approval text and add auto-decline feature

--- tgbot/handlers/group_approval.py
+++ tgbot/handlers/group_approval.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from aiogram import Bot, Router, types
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State
@@ -10,9 +12,9 @@ GroupApprovalState = State("group_approval_state")
 
 REACTIONS = ["👍", "👎", "❤", "🔥", "🥰", "👏"]
 GROUP_APPROVE_TEXT = {
-    "en": "Welcome to our group! Please react to this message with any of the following reactions 👍, 👎, ❤, 🔥, 🥰, 👏 to be approved. \n\nThis is a special approval function to keep the group spam-free.",
-    "ru": "Добро пожаловать в нашу группу! Пожалуйста, поставьте реакцию на это сообщение одним из следующих эмодзи 👍, 👎, ❤, 🔥, 🥰, 👏 для подтверждения. \n\nЭто специальная функция одобрения для защиты группы от спама.",
-    "uk": "Ласкаво просимо до нашої групи! Будь ласка, відреагуйте на це повідомлення будь-якою з наступних реакцій 👍, 👎, ❤, 🔥, 🥰, 👏 для схвалення. \n\nЦе спеціальна функція схвалення для захисту групи від спаму.",
+    "en": "Welcome to our group! Please react to this message with any of the following reactions 👍, 👎, ❤, 🔥, 🥰, 👏 to be approved. \n\nThis is a special approval function to keep the group spam-free.\n\nYou have 5 minutes and your request will be declined",
+    "ru": "Добро пожаловать в нашу группу! Пожалуйста, поставьте реакцию на это сообщение одним из следующих эмодзи 👍, 👎, ❤, 🔥, 🥰, 👏 для подтверждения. \n\nЭто специальная функция одобрения для защиты группы от спама.\n\nУ вас есть 5 минут, после чего заявка будет отклонена.",
+    "uk": "Ласкаво просимо до нашої групи! Будь ласка, відреагуйте на це повідомлення будь-якою з наступних реакцій 👍, 👎, ❤, 🔥, 🥰, 👏 для схвалення. \n\nЦе спеціальна функція схвалення для захисту групи від спаму.\n\nУ вас є 5 хвилин, після чого заявка буде скасована",
 }
 
 
@@ -37,6 +39,20 @@ async def group_enter_captcha(
     await state.storage.update_data(
         private_storage_key, dict(chat_id=chat_join_request.chat.id)
     )
+
+    await asyncio.sleep(280)
+    data = await state.storage.get_data(private_storage_key)
+    if data:
+        await bot.decline_chat_join_request(
+            chat_id=chat_join_request.chat.id,
+            user_id=chat_join_request.from_user.id,
+        )
+        await bot.send_message(
+            chat_id=chat_join_request.from_user.id,
+            text="Your request has been declined",
+        )
+        await state.storage.set_state(private_storage_key, None)
+        await state.storage.set_data(private_storage_key, {})
 
 
 @group_approval_router.message_reaction(GroupApprovalState)
